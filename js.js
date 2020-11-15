c87e03021bb705b5081aa71fee325ce1a7a2b5dd
maryamgh1988 <36102515+maryamgh1988@users.noreply.github.com>
2020-11-15 23:31:13 +0000

    add some css

---
+++ js.js
@@ -0,0 +1,11 @@
+function removeBlackPage(){
+    let blackpage = document.querySelector('#black-page');
+    
+    blackpage.style.opacity = '0';
+
+    
+    
+}
+
+document.querySelector('#pic-part').onclick = removeBlackPage;
+
