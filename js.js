7dc36097706e3cc995a6aa5f536dd982374f12ed
maryamgh1988 <36102515+maryamgh1988@users.noreply.github.com>
2020-11-28 22:03:54 +0000

    add navigation and basic js for it

--- js.js
+++ js.js
@@ -1,9 +1,20 @@
 function hideBackground(){
-    let shadowBackground = document.querySelector('#shadow-background');
+    let shadowBackground = document.querySelector('#temporary-background');
     
-    shadowBackground.style.opacity = '1';
-    shadowBackground.style['z-index'] = '0';
+    shadowBackground.style.opacity = '0';
+    shadowBackground.style['z-index'] = '-1';
 }
 
 document.querySelector('#pic-part').onclick = hideBackground;
 
+
+
+var currentpage=1;
+function nextpage(){
+    let pages=document.querySelectorAll('.text-part > div' );
+    
+
+}
+
+
+document.querySelector('#next').onclick = nextpage;
