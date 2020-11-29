a90796696c2e71409eaf7d43cf6e9af99f9a1266
maryamgh1988 <36102515+maryamgh1988@users.noreply.github.com>
2020-11-29 00:57:39 +0000

    add nextpage js function

--- js.js
+++ js.js
@@ -9,11 +9,18 @@ document.querySelector('#pic-part').onclick = hideBackground;
 
 
 
-var currentpage=1;
+var currentpageindex=0;
 function nextpage(){
     let pages=document.querySelectorAll('.text-part > div' );
-    
-
+  let myArr= Array.from(pages);
+  for(let i=0;i<myArr.length;i++){
+      myArr[i].style.display = 'none';
+  }
+  currentpageindex++;
+  if(currentpageindex>= myArr.length){
+      currentpageindex = 0;
+  }
+myArr[currentpageindex].style.display='flex';
 }
 
 
