fbecd9e0e7d571c15c7a061cf3f6a43a7701484c
maryamgh1988 <36102515+maryamgh1988@users.noreply.github.com>
2020-12-06 18:25:30 +0000

    add imagenext and imageprev functions

--- js.js
+++ js.js
@@ -49,29 +49,76 @@ document.querySelector('#prev').onclick = prevpage;
 
 
 /*.......................slideimages........................................................................*/
-let currentInnerIndex = 0;
+
+//when you press the next or previous buttons your old timeouts do not cancel so now you have switched slides, the old timeout will still trigger and then you've also set a new one so it will trigger as well all way within the range of time that you would like.
+
+// Instead of creating a timeout for each button click i would suggest using intervals and keeping track of it outside any functions.
+
+// When the user presses a forward or back button cancel that interval and create a new one so that you don't run into the issue you are having now where you basically have like 10 loops spamming your carousel at the same time.
+
+let TimeoutId;
+let spans;
+let images;
+let spanArr;
+let imageArr
 function imageCarousel(){
-    currentInnerIndex++;
-    let spans=document.querySelectorAll('#left-part > span');
-    let images=document.querySelectorAll('.images');
-    let spanArr= Array.from(spans);
-    let imageArr= Array.from(images);
-    for(let i=0;i<spanArr.length;i++){
-        spanArr[i].style.opacity = '0';
-        imageArr[i].style.display = 'none';
-    }
-  if(currentInnerIndex > spanArr.length){
-    currentInnerIndex=1;
+  spans=document.querySelectorAll('#left-part > span');
+  images=document.querySelectorAll('.images');
+  spanArr= Array.from(spans);
+  imageArr= Array.from(images);
+  if(currentInnerIndex==1){
+    document.querySelector('#horizental-arrow').style.opacity= 1;
   }
+  for(let i=0;i<spanArr.length;i++){
+      spanArr[i].style.opacity = '0';
+      imageArr[i].style.display = 'none';
+  }
+if(currentInnerIndex > spanArr.length){
+  currentInnerIndex=1;
+}
+if(currentInnerIndex<0){
+  currentInnerIndex = spanArr.length+2;
+}
+console.log(currentInnerIndex);
+}
+
+
+
+let currentInnerIndex = 0;
+function imageNext(){
+    currentInnerIndex++;
+    imageCarousel();
+  spanArr[currentInnerIndex-1].style.opacity='1';
+  imageArr[currentInnerIndex-1].style.display='block';   
+  TimeoutId = setTimeout(imageNext,2000);
+}
+ setTimeout(imageNext,3000);
+
+
+
+function shownext(){
+  clearTimeout(TimeoutId);
+  imageNext()
+}
+
+
+document.querySelector('#next-horiz').addEventListener('click',shownext )
+
+
+function  imageCarouselPrev(){
+  currentInnerIndex--;
+    imageCarousel();
   spanArr[currentInnerIndex-1].style.opacity='1';
   imageArr[currentInnerIndex-1].style.display='block';
+  TimeoutId = setTimeout(imageNext,2000);
     
-   
-    setTimeout(imageCarousel,5000);
 }
-setTimeout(imageCarousel,2000);
 
 
+function showPrev(){
+  clearTimeout(TimeoutId);
+  imageCarouselPrev();
 
+}
 
-
+document.querySelector('#prev-horiz').addEventListener('click',showPrev )
